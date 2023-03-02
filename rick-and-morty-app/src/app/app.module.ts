e38bd30b36042749aa2648159f63fb16cd7c3905
Mts-Carneiro <mtscrn21@gmail.comgit config --global user.email mtscrn21@gmail.com>
2023-03-02 12:24:24 -0300

    Feat: Create header and seach bar

--- rick-and-morty-app/src/app/app.module.ts
+++ rick-and-morty-app/src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ListDataComponent } from './list-data/list-data.component';
 import { CharacterCardComponent } from './character-card/character-card.component';
+import { HeaderComponent } from './header/header.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ListDataComponent,
-    CharacterCardComponent
+    CharacterCardComponent,
+    HeaderComponent,
+    SearchBarComponent
   ],
   imports: [
     BrowserModule
